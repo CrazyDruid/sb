44b7924dbcc9fb94078023c08c0b67e981c15293
CrazyDruid <778528025@qq.com>
2019-05-23 15:27:13 +0800

    111

--- src/components/study/test12.vue
+++ src/components/study/test12.vue
@@ -7,6 +7,7 @@
 
     <div class="tooltip" v-on:click.stop v-if="show_tooltip">
 
+
       <!-- v-model 绑定了文本域的内容
        在文本域内容改变时，对应的变量也会实时改变  -->
       <input type="text" v-model="text_content" />
